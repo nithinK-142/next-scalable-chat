8c2895ce8072aa588a764e774e63fe3c82fad3b1
nithinK-142 <nithin.sagar.359@gmail.com>
2023-12-14 18:17:31 +0530

    update: event message emit successfull.

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -6,8 +6,9 @@ import {
   useCallback,
   useContext,
   useEffect,
+  useState,
 } from "react";
-import { io } from "socket.io-client";
+import { Socket, io } from "socket.io-client";
 
 interface SocketProviderProps {
   children?: ReactNode;
@@ -27,15 +28,23 @@ export const useSocket = () => {
 };
 
 export const SocketProvider: FC<SocketProviderProps> = ({ children }) => {
-  const sendMessage: ISocketContext["sendMessage"] = useCallback((msg) => {
-    console.log(msg);
-  }, []);
+  const [socket, setSocket] = useState<Socket>();
+  const sendMessage: ISocketContext["sendMessage"] = useCallback(
+    (msg) => {
+      console.log(msg);
+      if (socket) {
+        socket.emit("event:message", { message: msg });
+      }
+    },
+    [socket]
+  );
 
   useEffect(() => {
     const _socket = io("http://localhost:8000");
-
+    setSocket(_socket);
     return () => {
       _socket.disconnect();
+      setSocket(undefined);
     };
   }, []);
   return (
