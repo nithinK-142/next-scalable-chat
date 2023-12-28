aaf5a8779264f01659bd57e0c7892b3a7df09b38
nithinK-142 <nithin.sagar.359@gmail.com>
2023-12-28 21:29:08 +0530

    update: kafka consumes messages and throws in postgres.

--- apps/server/src/services/kafka.ts
+++ apps/server/src/services/kafka.ts
@@ -2,10 +2,11 @@ import { Kafka, Producer } from "kafkajs";
 import fs from "fs";
 import path from "path";
 import { config } from "dotenv";
+import prismaClient from "./prisma";
 
 config();
 
-const kafa = new Kafka({
+const kafka = new Kafka({
   brokers: [`${process.env.KAFKA_URL}`],
   ssl: {
     ca: [fs.readFileSync(path.resolve("./ca.pem")), "utf-8"],
@@ -21,7 +22,7 @@ let producer: null | Producer = null;
 
 async function createProducer() {
   if (producer) return producer;
-  const _producer = kafa.producer();
+  const _producer = kafka.producer();
   await _producer.connect();
   producer = _producer;
   return producer;
@@ -36,4 +37,31 @@ export async function produceMessage(message: string) {
   return true;
 }
 
-export default kafa;
+export async function startMessageConsumer() {
+  const consumer = kafka.consumer({ groupId: "default" });
+  await consumer.connect();
+  await consumer.subscribe({ topic: "MESSAGES" });
+
+  await consumer.run({
+    autoCommit: true,
+    eachMessage: async ({ message, pause }) => {
+      if (!message.value) return;
+      console.log("New message received...");
+      try {
+        await prismaClient.message.create({
+          data: {
+            text: message.value?.toString(),
+          },
+        });
+      } catch (err) {
+        console.log("Holt your titts for a bit!!");
+        pause();
+        setTimeout(() => {
+          consumer.resume([{ topic: "MESSAGES" }]);
+        }, 60 * 1000);
+      }
+    },
+  });
+}
+
+export default kafka;
